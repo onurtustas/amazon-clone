cfd58f43f11d7bd386e2ba7e015a6d7f3f1f5c82
onurtustas <33491291+onurtustas@users.noreply.github.com>
2021-10-20 10:20:22 +0300

    prettier

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
 import Checkout from "./Checkout";
-
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
